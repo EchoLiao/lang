4cd01bf8d1dd484f0ca49ef8baeda0299a1a751d
tge3636 <tge3636@126.com>
2011-11-21 14:37:02 +0800

    使能编译通过并能运行

--- books/c/graph/CGwO.Hearn/Chap5ProgramsTxt/prog5RotateRoutines3D.P227.cpp
+++ books/c/graph/CGwO.Hearn/Chap5ProgramsTxt/prog5RotateRoutines3D.P227.cpp
@@ -1,3 +1,15 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*  Set initial display-window size. */
+GLsizei winWidth = 600, winHeight = 600;
+
+/*  Set range for world coordinates.  */
+GLfloat xwcMin = -150.0, xwcMax = 150.0;
+GLfloat ywcMin = -150.0, ywcMax = 150.0;
+
+
 class wcPt3D {
     public:
         GLfloat x, y, z;
@@ -5,6 +17,13 @@ class wcPt3D {
 typedef float Matrix4x4 [4][4];
 
 Matrix4x4 matRot;
+
+
+void init (void)
+{
+    /*  Set color of display window to white.  */
+    glClearColor (1.0, 1.0, 1.0, 0.0);
+}
 
 /* Construct the 4 by 4 identity matrix. */
 void matrix4x4SetIdentity (Matrix4x4 matIdent4x4)
@@ -17,6 +36,8 @@ void matrix4x4SetIdentity (Matrix4x4 matIdent4x4)
 }
 
 /* Premultiply matrix m1 times matrix m2, store result in m2. */
+// m2 = m1 * m2
+// 行主序.
 void matrix4x4PreMultiply (Matrix4x4 m1, Matrix4x4 m2)
 {
     GLint row, col;
@@ -86,14 +107,95 @@ void rotate3D (wcPt3D p1, wcPt3D p2, GLfloat radianAngle)
     translate3D (p1.x, p1.y, p1.z);
 }
 
+void transformVerts3D (GLint nVerts, wcPt3D * verts)
+{
+    GLint k;
+    GLfloat tempX, tempY;
+
+    for (k = 0; k < nVerts; k++) {
+        tempX = matRot [0][0] * verts [k].x +
+            matRot [0][1] * verts [k].y +
+            matRot [0][2] * verts [k].z +
+            matRot [0][3];
+        tempY = matRot [1][0] * verts [k].x +
+            matRot [1][1] * verts [k].y +
+            matRot [1][2] * verts [k].z +
+            matRot [1][3];
+        verts [k].z = matRot [2][0] * verts [k].x +
+            matRot [2][1] * verts [k].y +
+            matRot [2][2] * verts [k].z +
+            matRot [2][3];
+        verts [k].x = tempX;
+        verts [k].y = tempY;
+    }
+}
+
+void triangle (wcPt3D *verts)
+{
+    GLint k;
+
+    glBegin (GL_TRIANGLES);
+    for (k = 0; k < 3; k++)
+        glVertex3f (verts[k].x, verts[k].y, verts[k].z);
+    glEnd ( );
+}
+
 void displayFcn (void)
 {
     /*  Input rotation parameters.  */
+    GLint nVerts = 3;
+    wcPt3D verts[3] = {
+        {-100.0, 0.0, 0.0},
+        { 100.0, 0.0, 0.0},
+        { 0.0, 100.0, 0.0},
+    };
+
+    glColor3f (0.0, 0.0, 1.0);      //  Set initial fill color to blue.
+    triangle (verts);               //  Display blue triangle.
 
     /*  Initialize matRot to identity matrix:  */
     matrix4x4SetIdentity (matRot);
 
     /*  Pass rotation parameters to procedure rotate3D.  */
+    // N = Np2 - Np1
+    wcPt3D Np1 = {1.0, 0.0, 0.0};
+    wcPt3D Np2 = {0.0, 0.0, 0.0};
+    rotate3D(Np1, Np2, 60 * (3.1415926 / 180.0));
 
     /*  Display rotated object.  */
+    transformVerts3D(nVerts, &verts[0]);
+    glColor3f (1.0, 0.0, 0.0);      //  Set initial fill color to blue.
+    triangle (verts);               //  Display blue triangle.
+
+    glFlush();
+}
+
+void winReshapeFcn (GLint newWidth, GLint newHeight)
+{
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ( );
+    glOrtho(xwcMin, xwcMax, ywcMin, ywcMax, 10, 600);
+
+    glMatrixMode (GL_MODELVIEW);
+    glLoadIdentity ( );
+    glTranslatef(0.0, 0.0, -10.0);
+
+    glClear (GL_COLOR_BUFFER_BIT);
+}
+
+int main (int argc, char ** argv)
+{
+    glutInit (&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition (50, 50);
+    glutInitWindowSize (winWidth, winHeight);
+    glutCreateWindow ("Geometric Transformation Sequence");
+
+    init ( );
+    glutDisplayFunc (displayFcn);
+    glutReshapeFunc (winReshapeFcn);
+
+    glutMainLoop ( );
+
+    return 0;
 }
