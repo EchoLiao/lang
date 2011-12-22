c706648d607dbd25806c04ee6356742b8a6db093
tge3636 <tge3636@126.com>
2011-12-22 14:37:37 +0800

    some comments

--- books/c/graph/NeHe/LESSON43/FreeType.cpp
+++ books/c/graph/NeHe/LESSON43/FreeType.cpp
@@ -12,6 +12,7 @@ namespace freetype {
 
 ///This function gets the first power of 2 >= the
 ///int that we pass it.
+// 这个函数返回比a大的，并且是最接近a的2的次方的数
 inline int next_p2 ( int a )
 {
 	int rval=1;
@@ -26,26 +27,36 @@ void make_dlist ( FT_Face face, char ch, GLuint list_base, GLuint * tex_base ) {
 	//into a bitmap.  This actually requires a couple of FreeType commands:
 
 	//Load the Glyph for our character.
+    // 载入给定字符的轮廓
 	if(FT_Load_Glyph( face, FT_Get_Char_Index( face, ch ), FT_LOAD_DEFAULT ))
 		throw std::runtime_error("FT_Load_Glyph failed");
 
 	//Move the face's glyph into a Glyph object.
+    // 保存轮廓对象
     FT_Glyph glyph;
     if(FT_Get_Glyph( face->glyph, &glyph ))
 		throw std::runtime_error("FT_Get_Glyph failed");
 
 	//Convert the glyph to a bitmap.
+    // 把轮廓转化为位图
 	FT_Glyph_To_Bitmap( &glyph, ft_render_mode_normal, 0, 1 );
     FT_BitmapGlyph bitmap_glyph = (FT_BitmapGlyph)glyph;
 
 	//This reference will make accessing the bitmap easier
+    // 保存位图
 	FT_Bitmap& bitmap=bitmap_glyph->bitmap;
 
 	//Use our helper function to get the widths of
 	//the bitmap data that we will need in order to create
 	//our texture.
+    // 在OpenGL中纹理大小必须为2的次方. 
 	int width = next_p2( bitmap.width );
 	int height = next_p2( bitmap.rows );
+
+    /*
+       在FreeType中我们使用8位的颜色表示位图, 所以 FreeType 的位图可以保存亮度
+       信息.
+     */
 
 	//Allocate memory for the texture data.
     // 申请两倍大小的原因: 我们使用的是 GL_LUMINANCE_ALPHA !
@@ -59,6 +70,7 @@ void make_dlist ( FT_Face face, char ch, GLuint list_base, GLuint * tex_base ) {
 	//We use the ?: operator so that value which we use
 	//will be 0 if we are in the padding zone, and whatever
 	//is the the Freetype bitmap otherwise.
+    // 这里我们使用8位表示亮度, 8位表示alpha值
 	for(int j=0; j <height;j++) {
 		for(int i=0; i < width; i++){
 			expanded_data[2*(i+j*width)]= expanded_data[2*(i+j*width)+1] = 
@@ -83,7 +95,7 @@ void make_dlist ( FT_Face face, char ch, GLuint list_base, GLuint * tex_base ) {
     delete [] expanded_data;
 
 	//So now we can create the display list
-	glNewList(list_base+ch,GL_COMPILE);
+	glNewList(list_base+ch,GL_COMPILE); // NOTE1
 
 	glBindTexture(GL_TEXTURE_2D,tex_base[(int)ch]);
 
@@ -113,6 +125,17 @@ void make_dlist ( FT_Face face, char ch, GLuint list_base, GLuint * tex_base ) {
 	//oriented quite like we would like it to be,
 	//so we need to link the texture to the quad
 	//so that the result will be properly aligned.
+    /*
+     *      ^
+     *      |      (1,1)                      (w,0)
+     * (0,1)|--------             (0,0)|----------->
+     *      |       |                  |       |
+     *      |       |(1,0)             |       |(w,r)
+     * (0,0)|-------|--->         (0,r)|--------
+     *    <Texture Cord>               |
+     *                                 v  <Freetype Bitmap Cord>
+     */
+    // 绘制一个正方形, 显示字符
 	glBegin(GL_QUADS);
 	glTexCoord2d(0,0); glVertex2f(0,bitmap.rows);
 	glTexCoord2d(0,y); glVertex2f(0,0);
@@ -120,6 +143,8 @@ void make_dlist ( FT_Face face, char ch, GLuint list_base, GLuint * tex_base ) {
 	glTexCoord2d(x,0); glVertex2f(bitmap.width,bitmap.rows);
 	glEnd();
 	glPopMatrix();
+
+    // 移动光标到下一个位置, 以画下一个字符
 	glTranslatef(face->glyph->advance.x >> 6 ,0,0);
 
 
@@ -140,17 +165,20 @@ void font_data::init(const char * fname, unsigned int h) {
 	this->h=h;
 
 	//Create and initilize a freetype font library.
+    // 创建FreeType库
 	FT_Library library;
 	if (FT_Init_FreeType( &library )) 
 		throw std::runtime_error("FT_Init_FreeType failed");
 
 	//The object in which Freetype holds information on a given
 	//font is called a "face".
+    // 在FreeType库中保存字体信息的类叫做face
 	FT_Face face;
 
 	//This is where we load in the font information from the file.
 	//Of all the places where the code might die, this is the most likely,
 	//as FT_New_Face will die if the font file does not exist or is somehow broken.
+    // 使用输入的Freetype字符文件初始化face类
 	if (FT_New_Face( library, fname, 0, &face )) 
 		throw std::runtime_error("FT_New_Face failed (there is probably a problem with your font file)");
 
@@ -158,6 +186,7 @@ void font_data::init(const char * fname, unsigned int h) {
 	//in terms of 1/64ths of pixels.  Thus, to make a font
 	//h pixels high, we need to request a size of h*64.
 	//(h << 6 is just a prettier way of writting h*64)
+    // 在FreeType中使用1/64作为一个像素的高度所以我们需要缩放h来满足这个要求
 	FT_Set_Char_Size( face, h << 6, h << 6, 96, 96);
 
 	//Here we ask opengl to allocate resources for
@@ -188,122 +217,113 @@ void font_data::clean() {
 /// a projection matrix that will make object world 
 /// coordinates identical to window coordinates.
 inline void pushScreenCoordinateMatrix() {
-	glPushAttrib(GL_TRANSFORM_BIT);
-	GLint	viewport[4];
-	glGetIntegerv(GL_VIEWPORT, viewport);
-	glMatrixMode(GL_PROJECTION);
-	glPushMatrix();
-	glLoadIdentity();
-	gluOrtho2D(viewport[0],viewport[2],viewport[1],viewport[3]);
-	glPopAttrib();
+    glPushAttrib(GL_TRANSFORM_BIT); {
+        GLint	viewport[4];
+        glGetIntegerv(GL_VIEWPORT, viewport);
+        glMatrixMode(GL_PROJECTION);
+        glPushMatrix(); // PM1
+
+        glLoadIdentity();
+        gluOrtho2D(viewport[0],viewport[2],viewport[1],viewport[3]);
+    } glPopAttrib();
 }
 
 /// Pops the projection matrix without changing the current
 /// MatrixMode.
 inline void pop_projection_matrix() {
-	glPushAttrib(GL_TRANSFORM_BIT);
-	glMatrixMode(GL_PROJECTION);
-	glPopMatrix();
-	glPopAttrib();
+	glPushAttrib(GL_TRANSFORM_BIT); {
+        glMatrixMode(GL_PROJECTION);
+        glPopMatrix(); // PM1
+    } glPopAttrib();
 }
 
 ///Much like Nehe's glPrint function, but modified to work
 ///with freetype fonts.
-void print(const font_data &ft_font, float x, float y, const char *fmt, ...)  {
-	
-	// We want a coordinate system where things coresponding to window pixels.
-	pushScreenCoordinateMatrix();					
-	
-	GLuint font=ft_font.list_base;
-	float h=ft_font.h/.63f;						//We make the height about 1.5* that of
-	
-	char		text[256];								// Holds Our String
-	va_list		ap;										// Pointer To List Of Arguments
+void print(const font_data &ft_font, float x, float y, const char *fmt, ...)  
+{
+    // We want a coordinate system where things coresponding to window pixels.
+    // 画字时, 我们选择正交投影
+    pushScreenCoordinateMatrix();					
 
-	if (fmt == NULL)									// If There's No Text
-		*text=0;											// Do Nothing
+    GLuint font=ft_font.list_base;
+    // 使行与行之间有一定的空隙
+    float h=ft_font.h/.63f;						//We make the height about 1.5* that of
 
-	else {
-	va_start(ap, fmt);									// Parses The String For Variables
-	    vsprintf(text, fmt, ap);						// And Converts Symbols To Actual Numbers
-	va_end(ap);											// Results Are Stored In Text
-	}
+    char		text[256];						// Holds Our String
+    va_list		ap;										// Pointer To List Of Arguments
 
+    if (fmt == NULL)									// If There's No Text
+        *text=0;											// Do Nothing
+    else {
+        va_start(ap, fmt);
+        vsprintf(text, fmt, ap);
+        va_end(ap);
+    }
 
-	//Here is some code to split the text that we have been
-	//given into a set of lines.
-	//This could be made much neater by using
-	//a regular expression library such as the one avliable from
-	//boost.org (I've only done it out by hand to avoid complicating
-	//this tutorial with unnecessary library dependencies).
-	const char *start_line=text;
-	vector<string> lines;
+    //Here is some code to split the text that we have been
+    //given into a set of lines.
+    //This could be made much neater by using
+    //a regular expression library such as the one avliable from
+    //boost.org (I've only done it out by hand to avoid complicating
+    //this tutorial with unnecessary library dependencies).
+    const char *start_line=text;
+    vector<string> lines; // 用于保存提取出来的各行
+    const char * c = text;
+    for(;*c;c++) {
+        if(*c=='\n') {
+            string line;
+            for(const char *n=start_line;n<c;n++) line.append(1,*n);
+            lines.push_back(line);
+            start_line=c+1;
+        }
+    }
+    if(start_line) {
+        string line;
+        for(const char *n=start_line;n<c;n++) line.append(1,*n);
+        lines.push_back(line);
+    }
 
-	const char * c = text;
+    glPushAttrib(GL_LIST_BIT | GL_CURRENT_BIT  | GL_ENABLE_BIT | GL_TRANSFORM_BIT); {
+        glMatrixMode(GL_MODELVIEW);
+        glDisable(GL_LIGHTING);
+        glEnable(GL_TEXTURE_2D);
+        glDisable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);	
 
-	//for(const char *c=text;*c;c++) 
-	for(;*c;c++) {
-		if(*c=='\n') {
-			string line;
-			for(const char *n=start_line;n<c;n++) line.append(1,*n);
-			lines.push_back(line);
-			start_line=c+1;
-		}
-	}
-	if(start_line) {
-		string line;
-		for(const char *n=start_line;n<c;n++) line.append(1,*n);
-		lines.push_back(line);
-	}
+        glListBase(font);
 
-	glPushAttrib(GL_LIST_BIT | GL_CURRENT_BIT  | GL_ENABLE_BIT | GL_TRANSFORM_BIT);	
-	glMatrixMode(GL_MODELVIEW);
-	glDisable(GL_LIGHTING);
-	glEnable(GL_TEXTURE_2D);
-	glDisable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);	
+        float modelview_matrix[16];	
+        glGetFloatv(GL_MODELVIEW_MATRIX, modelview_matrix);
 
-	glListBase(font);
+        //This is where the text display actually happens.
+        //For each line of text we reset the modelview matrix
+        //so that the line's text will start in the correct position.
+        //Notice that we need to reset the matrix, rather than just translating
+        //down by h. This is because when each character is
+        //draw it modifies the current matrix so that the next character
+        //will be drawn immediatly after it.  
+        for(unsigned int i=0;i<lines.size();i++) {
+            glPushMatrix(); {
+                glLoadIdentity();
+                glTranslatef(x,y-h*i,0);
+                glMultMatrixf(modelview_matrix);
 
-	float modelview_matrix[16];	
-	glGetFloatv(GL_MODELVIEW_MATRIX, modelview_matrix);
+                //  The commented out raster position stuff can be useful if you need to
+                //  know the length of the text that you are creating.
+                //  If you decide to use it make sure to also uncomment the glBitmap command
+                //  in make_dlist().
+                //	glRasterPos2f(0,0);
+                // glCallLists() 会修改当前矩阵!     // NOTE1
+                glCallLists(lines[i].length(), GL_UNSIGNED_BYTE, lines[i].c_str());
+                //	float rpos[4];
+                //	glGetFloatv(GL_CURRENT_RASTER_POSITION ,rpos);
+                //	float len=x-rpos[0];
+            } glPopMatrix();
+        }
+    } glPopAttrib();		
 
-	//This is where the text display actually happens.
-	//For each line of text we reset the modelview matrix
-	//so that the line's text will start in the correct position.
-	//Notice that we need to reset the matrix, rather than just translating
-	//down by h. This is because when each character is
-	//draw it modifies the current matrix so that the next character
-	//will be drawn immediatly after it.  
-	for(unsigned int i=0;i<lines.size();i++) {
-		
-
-		glPushMatrix();
-		glLoadIdentity();
-		glTranslatef(x,y-h*i,0);
-		glMultMatrixf(modelview_matrix);
-
-	//  The commented out raster position stuff can be useful if you need to
-	//  know the length of the text that you are creating.
-	//  If you decide to use it make sure to also uncomment the glBitmap command
-	//  in make_dlist().
-	//	glRasterPos2f(0,0);
-		glCallLists(lines[i].length(), GL_UNSIGNED_BYTE, lines[i].c_str());
-	//	float rpos[4];
-	//	glGetFloatv(GL_CURRENT_RASTER_POSITION ,rpos);
-	//	float len=x-rpos[0];
-
-		glPopMatrix();
-
-		
-
-	}
-
-
-	glPopAttrib();		
-
-	pop_projection_matrix();
+    pop_projection_matrix();
 }
 
 }
