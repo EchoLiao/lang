a12cd185ce4be47af3fc1436ac1992ab99309df1
nuoerlz <nuoerlz@gmail.com>
2012-06-01 20:58:47 +0800

    使能输出中文

--- books/python/a-byte-of-python/helloworld.py
+++ books/python/a-byte-of-python/helloworld.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 # Filename : helloworld.py
+# a-byte-of-python/woodpecker.org.cn/abyteofpython_cn/chinese/ch03s04.html
+
+
 print 'Hello World'
+print 'Hello World! 世界!'
 
