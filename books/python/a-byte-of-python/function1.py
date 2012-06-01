e3174c807e998758f7a73de54e40d2478e76d318
nuoerlz <nuoerlz@gmail.com>
2012-06-01 22:16:01 +0800

    函数通过 def 关键字定义. def关键字后跟一个函数的 标识符 名称, 然后跟一对圆括 号. 圆括号之中可以包括一些变量名, 该行以冒号结尾. 接下来是一块语句, 它们是函 数体.

--- books/python/a-byte-of-python/function1.py
+++ books/python/a-byte-of-python/function1.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: function1.py
+
+
+
+# 函数通过 def 关键字定义. def关键字后跟一个函数的 标识符 名称, 然后跟一对圆括
+# 号. 圆括号之中可以包括一些变量名, 该行以冒号结尾. 接下来是一块语句, 它们是函
+# 数体.
 
 def sayHello():
 	print 'Hello World!' # block belonging to the function
