09b9cde89e697cd8eafa2a9ca3cb5b9a908727db
nuoerlz <nuoerlz@gmail.com>
2012-06-01 22:13:52 +0800

    continue语句被用来告诉Python跳过当前循环块中的剩余语句, 然后 继续 进行 下一轮循环.

--- books/python/a-byte-of-python/continue.py
+++ books/python/a-byte-of-python/continue.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: continue.py
+
+
+# continue语句被用来告诉Python跳过当前循环块中的剩余语句, 然后 继续 进行
+# 下一轮循环. 
 
 while True:
 	s=raw_input('Enter something : ')
