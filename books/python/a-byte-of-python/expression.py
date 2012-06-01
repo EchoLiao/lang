f94338a1c07d6d634ad469c6a8aa0e38041584a5
nuoerlz <nuoerlz@gmail.com>
2012-06-01 21:14:44 +0800

    注意Python如何打印"漂亮的"输出. 尽管我们没有在'Area is'和变量area之间指定空 格, Python自动在那里放了一个空格, 这样我们就可以得到一个清晰漂亮的输出, 而程 序也变得更加易读(因为我们不需要担心输出之间的空格问题).

--- books/python/a-byte-of-python/expression.py
+++ books/python/a-byte-of-python/expression.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Filename: expression.py
+
+# 注意Python如何打印"漂亮的"输出. 尽管我们没有在'Area is'和变量area之间指定空
+# 格, Python自动在那里放了一个空格, 这样我们就可以得到一个清晰漂亮的输出, 而程
+# 序也变得更加易读(因为我们不需要担心输出之间的空格问题).
+
 
 length=5
 breadth=2
