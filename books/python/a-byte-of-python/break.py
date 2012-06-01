023c2e4b736fc77dc6bdf4ccb17f8927fb633543
nuoerlz <nuoerlz@gmail.com>
2012-06-01 22:11:18 +0800

    break语句是用来 终止 循环语句的, 即哪怕循环条件没有称为False或序列还没有被完 全递归, 也停止执行循环语句.
    NOTES:  一个重要的注释是, 如果你从for或while循环中 终止 ,任何对应的循环else
            块将不执行!!!!!

--- books/python/a-byte-of-python/break.py
+++ books/python/a-byte-of-python/break.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: break.py
+
+# break语句是用来 终止 循环语句的, 即哪怕循环条件没有称为False或序列还没有被完
+# 全递归, 也停止执行循环语句.
+
+# NOTES: 一个重要的注释是, 如果你从for或while循环中 终止 , 任何对应的循环else
+# 块将不执行!!!!!
 
 while True:
 	s=raw_input('Enter something : ')
