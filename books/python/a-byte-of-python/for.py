8c6ffadcd122713c95c14693dd319a992a049a50
nuoerlz <nuoerlz@gmail.com>
2012-06-01 22:05:06 +0800

    在C/C++中, 写for (int i = 0; i < 5; i++); 那么用Python, 你写成 for i in range(0,5). 你会注意到, Python的for循环更加简单,明白,不易出错.

---
+++ books/python/a-byte-of-python/for.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Filename: for.py
+
+
+# 在C/C++中, 写for (int i = 0; i < 5; i++); 那么用Python, 你写成
+# for i in range(0,5). 你会注意到, Python的for循环更加简单,明白,不易出错.  
+
+for i in range(1, 5):
+    print i
+else:
+    print 'The for loop is over'
