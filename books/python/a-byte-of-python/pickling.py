9e98d2454dcf902026c4ef2d57942667bae37bcd
nuoerlz <nuoerlz@gmail.com>
2012-06-02 11:10:27 +0800

    储存器:
    Python提供一个标准的模块, 称为pickle.  使用它你可以在一个文件中储存任何Python
    对象, 之后你又可以把它完整无缺地取出来. 这被称为 持久地 储存对象.
    
    还有另一个模块称为cPickle, 它的功能和pickle模块完全相同,只不过它是用C语言编写
    的, 因此要快得多(比pickle快1000倍). 你可以使用它们中的任一个,而我们在这里将使
    用cPickle模块. 记住, 我们把这两个模块都简称为pickle模块.

--- books/python/a-byte-of-python/pickling.py
+++ books/python/a-byte-of-python/pickling.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: pickling.py
+
+
+# 储存器:
+
+# Python提供一个标准的模块, 称为pickle. 使用它你可以在一个文件中储存任何Python
+# 对象, 之后你又可以把它完整无缺地取出来. 这被称为 持久地 储存对象. 
+
+# 还有另一个模块称为cPickle, 它的功能和pickle模块完全相同, 只不过它是用C语言编
+# 写的, 因此要快得多(比pickle快1000倍). 你可以使用它们中的任一个, 而我们在这里
+# 将使用cPickle模块. 记住, 我们把这两个模块都简称为pickle模块. 
+
 
 import cPickle as p
 #import pickle as p
