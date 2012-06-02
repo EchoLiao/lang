91430a086718f018cc09671a8297db8074963583
nuoerlz <nuoerlz@gmail.com>
2012-06-02 11:20:27 +0800

    try..finally 假如你在读一个文件的时候, 希望在无论异常发生与否的情况下都关闭文件, 该怎么做 呢?这可以使用finally块来完成. 注意, 在一个try块下, 你可以同时使用except从句 和 finally块. 如果你要同时使用它们的话, 需要把一个嵌入另外一个.

--- books/python/a-byte-of-python/finally.py
+++ books/python/a-byte-of-python/finally.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # Filename: finally.py
 
+
+# try..finally
+# 假如你在读一个文件的时候, 希望在无论异常发生与否的情况下都关闭文件, 该怎么做
+# 呢?这可以使用finally块来完成. 注意, 在一个try块下, 你可以同时使用except从句
+# 和finally块. 如果你要同时使用它们的话, 需要把一个嵌入另外一个. 
+
+
 import time
 
 try:
