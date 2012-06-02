7ee8db8ead7721177fa492e8e25903528b09d310
nuoerlz <nuoerlz@gmail.com>
2012-06-02 11:06:57 +0800

    文件: 你可以通过创建一个file类的对象来打开一个文件, 分别使用file类的read,readline 或 write方法来恰当地读写文件.  对文件的读写能力依赖于你在打开文件时指定的模式. 最后, 当你完成对文件的操作的时候, 你调用close方法来告诉Python我们完成了对文 件 的使用.

--- books/python/a-byte-of-python/using_file.py
+++ books/python/a-byte-of-python/using_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: using_file.py
+
+
+# 文件:
+# 你可以通过创建一个file类的对象来打开一个文件, 分别使用file类的read,readline
+# 或write方法来恰当地读写文件. 对文件的读写能力依赖于你在打开文件时指定的模式.
+# 最后, 当你完成对文件的操作的时候, 你调用close方法来告诉Python我们完成了对文
+# 件的使用. 
+
 
 poem='''\
 Programming is fun
