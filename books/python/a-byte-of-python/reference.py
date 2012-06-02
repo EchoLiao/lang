5c093d47dbe0a526631c76c77aa73035d51ba438
nuoerlz <nuoerlz@gmail.com>
2012-06-02 09:54:37 +0800

    参考: 当你创建一个对象并给它赋一个变量的时候, 这个变量仅仅 参考 那个对象, 而不是表 示这个对象本身!也就是说, 变量名指向你计算机中存储那个对象的内存.  这被称作名 称到对象的绑定.
    记住, 列表的赋值语句不创建拷贝. 你得使用切片操作符来建立序列的拷贝.

--- books/python/a-byte-of-python/reference.py
+++ books/python/a-byte-of-python/reference.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: reference.py
+
+
+# 参考:
+# 当你创建一个对象并给它赋一个变量的时候, 这个变量仅仅 参考 那个对象, 而不是表
+# 示这个对象本身!也就是说, 变量名指向你计算机中存储那个对象的内存. 这被称作名
+# 称到对象的绑定. 
+
+# 记住, 列表的赋值语句不创建拷贝. 你得使用切片操作符来建立序列的拷贝.  
+
 
 print 'Simple Assignment'
 shoplist=['apple','mango','carrot','banana']
