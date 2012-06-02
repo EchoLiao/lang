31b71ad9acf2c7afac18d6ec407beb74a8f99277
nuoerlz <nuoerlz@gmail.com>
2012-06-02 09:59:21 +0800

    字符串: 字符串都是str类的对象.
    str类也有以一个作为分隔符的字符串join序列的项目的整洁的方法, 它返回一个生成的
    大字符串.

--- books/python/a-byte-of-python/str_methods.py
+++ books/python/a-byte-of-python/str_methods.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: str_methods.py
+
+
+# 字符串:
+# 字符串都是str类的对象.
+
 
 name='Swaroop' # This is a string object
 
@@ -12,7 +18,9 @@ if 'a' in name:
 if name.find('war')!=-1:
 	print 'Yes, it contains the string "war"'
 
-delimiter=' * '
+# str类也有以一个作为分隔符的字符串join序列的项目的整洁的方法, 它返回一个生成
+# 的大字符串.
+delimiter=' * '     # 定义一个字符串
 mylist=['Brazil','Russia','India','China']
-print delimiter.join(mylist)
+print delimiter.join(mylist)        # 使用str类的join()方法.
 
