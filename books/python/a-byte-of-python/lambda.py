f2cb1d56ea28fe295458ca0e4b69f0e6a8db0db2
nuoerlz <nuoerlz@gmail.com>
2012-06-02 11:54:56 +0800

    lambda语句被用来创建新的函数对象, 并且在运行时返回它们.
    这里, 我们使用了make_repeater函数在运行时创建新的函数对象, 并且返回它.  lambda
    语句用来创建函数对象. 本质上, lambda需要一个参数, 后面仅跟单个表达式作 为函数
    体, 而表达式的值被这个新建的函数返回. 注意, 即便是print语句也不能用在 lambda形
    式中, 只能使用表达式.

--- books/python/a-byte-of-python/lambda.py
+++ books/python/a-byte-of-python/lambda.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: lambda.py
+
+
+# lambda语句被用来创建新的函数对象, 并且在运行时返回它们.
+
+# 这里, 我们使用了make_repeater函数在运行时创建新的函数对象, 并且返回它.
+# lambda语句用来创建函数对象. 本质上, lambda需要一个参数, 后面仅跟单个表达式作
+# 为函数体, 而表达式的值被这个新建的函数返回. 注意, 即便是print语句也不能用在
+# lambda形式中, 只能使用表达式.
+
 
 def make_repeater(n):
 	return lambda s: s*n
