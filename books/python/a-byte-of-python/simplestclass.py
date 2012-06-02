7fe5db50cab961b4c2b19a2f9934368fe2ef20a7
nuoerlz <nuoerlz@gmail.com>
2012-06-02 10:44:20 +0800

    我们使用class语句后跟类名, 创建了一个新的类.  这后面跟着一个缩进的语句块形成 类体. 在这个例子中, 我们使用了一个空白块, 它由pass语句表示.

--- books/python/a-byte-of-python/simplestclass.py
+++ books/python/a-byte-of-python/simplestclass.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: simplestclass.py
+
+
+# 我们使用class语句后跟类名, 创建了一个新的类. 这后面跟着一个缩进的语句块形成
+# 类体. 在这个例子中, 我们使用了一个空白块, 它由pass语句表示. 
+
 
 class Person:
 	pass # An empty block
