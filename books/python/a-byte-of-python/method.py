6361d84fa856ef015171e02515e8ee307baac7f1
nuoerlz <nuoerlz@gmail.com>
2012-06-02 10:47:58 +0800

    对象的方法: 我们已经讨论了类/对象可以拥有像函数一样的方法, 这些方法与函数的区别只是一个 额 外的self变量. 现在我们来学习一个例子.

--- books/python/a-byte-of-python/method.py
+++ books/python/a-byte-of-python/method.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Filename: method.py
+
+
+# 对象的方法:
+# 我们已经讨论了类/对象可以拥有像函数一样的方法, 这些方法与函数的区别只是一个
+# 额外的self变量. 现在我们来学习一个例子. 
+
 
 class Person:
 	def sayHi(self):
