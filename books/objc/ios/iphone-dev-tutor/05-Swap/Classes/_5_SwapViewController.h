d3abd7538de4fd3e2442026b26caee5e7a5347ca
nuoerlz <nuoerlz@gmail.com>
2012-06-13 23:22:45 +0800

    "切换视图"的方式实现旋转

--- books/objc/ios/iphone-dev-tutor/05-Swap/Classes/_5_SwapViewController.h
+++ books/objc/ios/iphone-dev-tutor/05-Swap/Classes/_5_SwapViewController.h
@@ -8,9 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
+#define degreesToRadian(x) (M_PI * (x) / 180.0)
+
 @interface _5_SwapViewController : UIViewController {
 
+	UIView    *landscape;
+    UIView    *portrait;
+    
+    // Foo
+    UIButton *landscapeFooButton;
+    UIButton *portraitFooButton;
+    
+    // Bar
+    UIButton *landscapeBarButton;
+    UIButton *portraitBarButton;
 }
+
+@property (nonatomic, retain) IBOutlet UIView *landscape;
+@property (nonatomic, retain) IBOutlet UIView *portrait;
+@property (nonatomic, retain) IBOutlet UIButton *landscapeFooButton;
+@property (nonatomic, retain) IBOutlet UIButton *portraitFooButton;
+@property (nonatomic, retain) IBOutlet UIButton *landscapeBarButton;
+@property (nonatomic, retain) IBOutlet UIButton *portraitBarButton;
+
+-(IBAction)buttonPressed:(id)sender;
 
 @end
 
--- books/objc/ios/iphone-dev-tutor/05-Swap/Classes/_5_SwapViewController.m
+++ books/objc/ios/iphone-dev-tutor/05-Swap/Classes/_5_SwapViewController.m
@@ -12,6 +12,65 @@
 
 
 
+@synthesize landscape;
+@synthesize portrait;
+@synthesize landscapeFooButton;
+@synthesize portraitFooButton;
+@synthesize landscapeBarButton;
+@synthesize portraitBarButton;
+
+-(IBAction)buttonPressed:(id)sender {
+	
+	if (sender == portraitFooButton || sender == landscapeFooButton) {
+		portraitFooButton.hidden = YES;
+		landscapeFooButton.hidden = YES;
+	}
+	else {
+		portraitBarButton.hidden = YES;
+		landscapeBarButton.hidden = YES;
+	}
+}
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)
+		interfaceOrientation duration:(NSTimeInterval)duration {	
+    if (interfaceOrientation == UIInterfaceOrientationPortrait)
+    {
+        self.view = self.portrait;
+        self.view.transform = CGAffineTransformIdentity;
+        self.view.transform = CGAffineTransformMakeRotation(degreesToRadian(0));
+        self.view.bounds = CGRectMake(0.0, 0.0, 300.0, 480.0);	
+    }
+    else if (interfaceOrientation == UIInterfaceOrientationLandscapeLeft)
+    {
+        self.view = self.landscape;
+        self.view.transform = CGAffineTransformIdentity;
+        self.view.transform = CGAffineTransformMakeRotation(degreesToRadian(-90));
+        self.view.bounds = CGRectMake(0.0, 0.0, 460.0, 320.0);
+    }
+    else if (interfaceOrientation == UIInterfaceOrientationPortraitUpsideDown)
+    {
+        self.view = self.portrait;
+        self.view.transform = CGAffineTransformIdentity;
+        self.view.transform = CGAffineTransformMakeRotation(degreesToRadian(180));
+        self.view.bounds = CGRectMake(0.0, 0.0, 300.0, 480.0);
+    }
+    else if (interfaceOrientation == UIInterfaceOrientationLandscapeRight)
+    {
+        self.view = self.landscape;
+        self.view.transform = CGAffineTransformIdentity;
+        self.view.transform = 
+        CGAffineTransformMakeRotation(degreesToRadian(90));
+        self.view.bounds = CGRectMake(0.0, 0.0, 460.0, 320.0);
+    }
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	return YES;
+}
+
+
+
+
 /*
 // The designated initializer. Override to perform setup that is required before the view is loaded.
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
@@ -56,10 +115,24 @@
 - (void)viewDidUnload {
 	// Release any retained subviews of the main view.
 	// e.g. self.myOutlet = nil;
+	self.landscape = nil;
+    self.portrait = nil;
+    self.landscapeFooButton = nil;
+    self.landscapeBarButton = nil;
+    self.portraitFooButton = nil;
+    self.portraitBarButton = nil;
+	[super viewDidUnload];
 }
 
 
 - (void)dealloc {
+	[landscape release];
+	[portrait release];
+	[landscapeFooButton release];	
+	[portraitFooButton release];
+	[landscapeBarButton release];
+	[portraitBarButton release];
+	
     [super dealloc];
 }
 
