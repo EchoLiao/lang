29c69435557beea2eac47bc7e6a6fee51db54dca
nuoerlz <nuoerlz@gmail.com>
2012-06-13 22:58:40 +0800

    实现用"移动控件"的方式实现旋转(要记得在"interface builder"中设置"Autosizing"属性

--- books/objc/ios/iphone-dev-tutor/05-AutosizeWithFrame/Classes/_5_AutosizeWithFrameViewController.h
+++ books/objc/ios/iphone-dev-tutor/05-AutosizeWithFrame/Classes/_5_AutosizeWithFrameViewController.h
@@ -9,8 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface _5_AutosizeWithFrameViewController : UIViewController {
-
+	UIButton *button1;
+    UIButton *button2;
+    UIButton *button3;
+    UIButton *button4;
+    UIButton *button5;
+    UIButton *button6;
 }
-
+@property (nonatomic, retain) IBOutlet UIView *button1;
+@property (nonatomic, retain) IBOutlet UIView *button2;
+@property (nonatomic, retain) IBOutlet UIView *button3;
+@property (nonatomic, retain) IBOutlet UIView *button4;
+@property (nonatomic, retain) IBOutlet UIView *button5;
+@property (nonatomic, retain) IBOutlet UIView *button6;
 @end
 
