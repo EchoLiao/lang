5b0852c01a13687e6f33dfdd146b730011e565c2
nuoerlz <nuoerlz@gmail.com>
2012-06-14 22:28:00 +0800

    添加一些必要文件

---
+++ books/objc/ios/iphone-dev-tutor/06-ViewSwitch/Classes/BlueViewController.h
@@ -0,0 +1,16 @@
+//
+//  BlueViewController.h
+//  06-ViewSwitch
+//
+//  Created by nuoerlz on 12-6-14.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface BlueViewController : UIViewController {
+
+}
+
+@end
