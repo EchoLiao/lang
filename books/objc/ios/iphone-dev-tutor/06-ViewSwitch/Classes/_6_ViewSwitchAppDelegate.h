9ca865727b190d62ae2cd0ab5d7b13367db3707a
nuoerlz <nuoerlz@gmail.com>
2012-06-14 22:12:38 +0800

    多视图应用程序

---
+++ books/objc/ios/iphone-dev-tutor/06-ViewSwitch/Classes/_6_ViewSwitchAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  _6_ViewSwitchAppDelegate.h
+//  06-ViewSwitch
+//
+//  Created by nuoerlz on 12-6-14.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _6_ViewSwitchAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
