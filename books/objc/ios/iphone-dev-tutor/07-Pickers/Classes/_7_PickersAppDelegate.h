3437277593ab28ec5ca589b1295cd3d1a7ac5e4c
nuoerlz <nuoerlz@gmail.com>
2012-06-17 09:03:03 +0800

    选择器的使用

---
+++ books/objc/ios/iphone-dev-tutor/07-Pickers/Classes/_7_PickersAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  _7_PickersAppDelegate.h
+//  07-Pickers
+//
+//  Created by nuoerlz on 12-6-17.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _7_PickersAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ books/objc/ios/iphone-dev-tutor/07-Pickers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  07-Pickers
+//
+//  Created by nuoerlz on 12-6-17.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
