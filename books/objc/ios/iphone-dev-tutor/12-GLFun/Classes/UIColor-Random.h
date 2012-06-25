d13a90f9d50ce3462e88cfead72ed471433412c6
nuoerlz <nuoerlz@gmail.com>
2012-06-25 20:59:14 +0800

    增加一些必要文件

---
+++ books/objc/ios/iphone-dev-tutor/12-GLFun/Classes/UIColor-Random.h
@@ -0,0 +1,16 @@
+//
+//  UIColor-Random.h
+//  12-QuartzFun
+//
+//  Created by nuoerlz on 12-6-20.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// 对UIColor类追加新方法。
+@interface UIColor(Random)
+
++(UIColor *)randomColor;
+
+@end
