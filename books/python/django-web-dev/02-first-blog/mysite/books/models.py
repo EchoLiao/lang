528bf0a605d813999d85525f8c40884ce1d20901
nuoerlz <nuoerlz@gmail.com>
2012-07-02 23:11:12 +0800

    当我们打印整个publisher列表时, 我们没有得到想要的有用的信息:     [<Publisher: Publisher object>, <Publisher: Publisher object>] 我们可以简单解决这个问题, 只需要添加一个方法 __str__() 到 Publisher 对象. __str__() 方法告诉Python要怎样把对象当作字符串来使用.

--- books/python/django-web-dev/02-first-blog/mysite/books/models.py
+++ books/python/django-web-dev/02-first-blog/mysite/books/models.py
@@ -22,6 +22,10 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     website = models.URLField()
 
+    def __str__(self):
+        return self.name
+
+
 
 class Author(models.Model):
     salutation = models.CharField(max_length=10)
@@ -30,10 +34,17 @@ class Author(models.Model):
     email = models.EmailField()
     headshot = models.ImageField(upload_to='/tmp')
 
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
 
