c29ff9bcbafa72be23f349ad988562578eda8316
nuoerlz <nuoerlz@gmail.com>
2012-07-02 21:09:13 +0800

    可以利用 Python 的内建函数 locals() . 它返回的字典对所有局部变量的名称与值进行 映射.
    使用 locals() 时要注意是它将包括 所有 的局部变量, 组成它的变量可能比你想让模板
    访问的要多. 在前例中,  locals() 还包含了 request . 对此如何取舍取决你的应用程序.

--- books/python/django-web-dev/02-first-blog/mysite/views.py
+++ books/python/django-web-dev/02-first-blog/mysite/views.py
@@ -6,12 +6,14 @@ import datetime
 
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    return render_to_response('current_datetime.html', {'current_date': now})
+    #now = datetime.datetime.now()
+    #return render_to_response('current_datetime.html', {'current_date': now})
+    current_date = datetime.datetime.now()
+    return render_to_response('current_datetime.html', locals())
 
 
 def hours_ahead(request, offset):
-    offset = int(offset)
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    return render_to_response('hours_ahead.html', {'off_set': offset, 'current_date': dt})
+    off_set = int(offset)
+    dt_date = datetime.datetime.now() + datetime.timedelta(hours=off_set)
+    return render_to_response('hours_ahead.html', locals())
 
