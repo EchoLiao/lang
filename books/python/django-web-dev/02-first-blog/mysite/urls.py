e728a3fca8203bad5c7f5e744fdb89c747d97611
tge3636 <tge3636@126.com>
2012-07-04 14:31:09 +0800

    使用"对象的通用视图"为所有的书籍创建一个列表页面

--- books/python/django-web-dev/02-first-blog/mysite/urls.py
+++ books/python/django-web-dev/02-first-blog/mysite/urls.py
@@ -2,21 +2,29 @@
 
 from django.conf.urls.defaults import *
 from mysite.views import current_datetime, hours_ahead
+from django.views.generic import list_detail
+from mysite.books.models import Publisher
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+
+publisher_info = {
+        "queryset" : Publisher.objects.all(),
+        }
+
 urlpatterns = patterns('',
-    (r'^time/$', current_datetime),
+    (r'^time/$', current_datetime), # 使用"对象"指定视图函数
     # 圆括号"()"表示其将作为一个参数(offset)来传递.
     # def hours_ahead(request, offset):
     (r'^time/plus/(\d{1,2})/$', hours_ahead),
 
-    (r'^search/$','mysite.books.views.search'),
+    (r'^search/$','mysite.books.views.search'), # 使用"字符串"指定视图函数
     (r'^contact/$', 'mysite.books.views.contact'),
     (r'^contact/thanks/$', 'mysite.books.views.thanks'),
     (r'^add_publisher/$', 'mysite.books.views.add_publisher'),
+    (r'^publishers/$', list_detail.object_list, publisher_info),
 
     # Example:
     (r'^blog/', include('mysite.blog.urls')),
