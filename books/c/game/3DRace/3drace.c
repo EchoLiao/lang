05c1554db67afc386aa8235c4eb3087f15c4e49f
tge3636 <tge3636@126.com>
2012-07-04 17:22:00 +0800

    move templates/books/ to books/templates/

--- books/c/game/3DRace/3drace.c
+++ books/c/game/3DRace/3drace.c
@@ -148,6 +148,9 @@ void render(void)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glLoadIdentity();
+    gluLookAt(1.0, 0.0, 0.0,
+            0.0, 0.0, -100.0, 
+            0.0, 1.0, 0.0);
     glTranslatef(0.0f, 0.0f, -0.0f);
 
     glPushMatrix(); {
