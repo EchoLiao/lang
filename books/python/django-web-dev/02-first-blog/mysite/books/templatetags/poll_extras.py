6689563a1649db3b4f076d62e116688e7cbf00a7
tge3636 <tge3636@126.com>
2012-07-05 10:07:55 +0800

    自定义模板标签:     分析直至另外一个块标签并保存内容

--- books/python/django-web-dev/02-first-blog/mysite/books/templatetags/poll_extras.py
+++ books/python/django-web-dev/02-first-blog/mysite/books/templatetags/poll_extras.py
@@ -1,9 +1,41 @@
+# -*- coding: utf-8 -*-
+
 from django import template
 
 
 register = template.Library()
 
 
+
+
+##################### 过滤器 ########################
+
 @register.filter(name='cut')
 def cut(value, arg):
     return value.replace(arg, '*')
+
+
+
+
+
+
+##################### 标签 ##########################
+
+# 例如, 这个自定义模板标签:  {% upper %} , 它把自己和 {% endupper %} 之间的所
+# 有内容都变成大写:
+#   {% upper %}
+#       This will appear in uppercase, {{ your_name }}.
+#   {% endupper %}
+@register.tag(name='upper')
+def do_upper(parser, token):
+    nodelist = parser.parse(('endupper',))
+    parser.delete_first_token()
+    return UpperNode(nodelist)
+
+class UpperNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+    def render(self, context):
+        # 对节点列表中的每个 Node简单的调用 render() .
+        output = self.nodelist.render(context)
+        return output.upper()
