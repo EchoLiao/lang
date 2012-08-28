0667990c0ba37a2e033603f5661fb0530a8ec529
nuoerlz <nuoerlz@gmail.com>
2012-08-28 23:06:07 +0800

    ObjC中的super关键字意思

--- books/objc/ios/Pro-Objective-C-Design-Patterns-for-iOS/978-1-4302-3330-5_Chung_Ch07_Singleton_code/Classes/Singleton.m
+++ books/objc/ios/Pro-Objective-C-Design-Patterns-for-iOS/978-1-4302-3330-5_Chung_Ch07_Singleton_code/Classes/Singleton.m
@@ -8,6 +8,17 @@
 
 #import "Singleton.h"
 
+/*
+The Keyword super
+
+Sometimes (quite often, in Cocoa programming) you want to override an inherited method but still
+access the overridden functionality. To do so, you’ll use the keyword super. Like self, the keyword
+super is something you send a message to. But its meaning has nothing to do with “this instance”
+or any other instance. The keyword super is class-based, and it means: “Start the search for
+messages I receive in the superclass of this class” (where “this class” is the class where the
+keyword super appears).
+*/
+
 
 @implementation Singleton
 
