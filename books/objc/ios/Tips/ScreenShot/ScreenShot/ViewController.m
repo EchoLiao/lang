7bef2de931772d5e66ada84d1db7a3dfb6239d06
nuoerlz <nuoerlz@gmail.com>
2012-08-30 17:38:20 +0800

    能显示图片

--- books/objc/ios/Tips/ScreenShot/ScreenShot/ViewController.m
+++ books/objc/ios/Tips/ScreenShot/ScreenShot/ViewController.m
@@ -5,6 +5,10 @@
 //  Created by erlz nuo on 12-8-30.
 //  Copyright (c) 2012年 erlz nuo. All rights reserved.
 //
+//
+// http://disanji.net/2010/12/18/ios-clip-and-display-part-of-pictures/
+//
+
 
 #import "ViewController.h"
 
@@ -14,6 +18,18 @@
 
 @implementation ViewController
 
+
+-(void)loadView {
+    [[UIApplication sharedApplication] setStatusBarHidden:YES
+                                            withAnimation: UIStatusBarAnimationSlide];
+    UIImageView *contentView = [[UIImageView alloc]
+                                initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+    UIImage *image = [UIImage imageNamed:@"iphone.png"];
+    [contentView setImage:image];
+    
+    self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+    [self.view addSubview:contentView];
+}
 
 - (void)viewDidLoad
 {
