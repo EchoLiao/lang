d8031476e5156db91dd5db85e82283b6db753ee9
nuoerlz <nuoerlz@gmail.com>
2012-11-08 18:35:25 +0800

    automake

---
+++ books/c/test/automake/main.c
@@ -0,0 +1,27 @@
+/*
+ * ===========================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2012/11/08 17时46分04秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  nuoerlz (nuoliu), nuoerlz@gmail.com
+ *        Company:  mhtt
+ *
+ *      CopyRight:  Reserve
+ *
+ * ===========================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+    printf("auto create makefile! ");
+    return 0;
+}
+
