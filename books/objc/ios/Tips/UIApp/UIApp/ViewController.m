c8a2cf057260e5e9116c21af6f67a13e47435444
nuoerlz <nuoerlz@gmail.com>
2013-01-07 16:22:12 +0800

    methods of open another app

--- books/objc/ios/Tips/UIApp/UIApp/ViewController.m
+++ books/objc/ios/Tips/UIApp/UIApp/ViewController.m
@@ -36,10 +36,44 @@
 
 - (IBAction)openBrowser:(id)sender
 {
+//    itms:// 可以打开MobileStore.app
+//    audio-player-event:// 可以打开iPod
+//    audio-player-event://?uicmd=show-purchased-playlist 可以打开iPod播放列表
+//    video-player-event:// 可以打开iPod中的视频
+
+
+    // app store
+//    NSURL *appStoreUrl = [NSURL URLWithString:@"http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=291586600&amp;mt=8"];
+//    [[UIApplication sharedApplication] openURL:appStoreUrl];
+
+    // message
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"sms:55555"]];
+
+    // phone
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tel://8004664411"]];
+
+    // mail
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto://info@iphonedevelopertips.com"]];
+
+    // google map
+//    NSString *searchQuery = @"the postion I want to know";
+//    searchQuery = [searchQuery stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding];
+//    NSString *urlString = [NSString stringWithFormat:@"http://maps.google.com/maps?q=%@", searchQuery];
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString: urlString]];
+
+    // browser
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.google.com/"]];
 }
 
 - (IBAction)openAnotherApp:(id)sender
 {
+    NSString *video = [NSString stringWithFormat:@"VPlayer://%@/like.mp4", kDocsPath];
+//    NSString *video = @"VPlayer://http://v.youku.com/v_show/id_XNDk3ODAzMzEy.html";
+//    NSString *video = @"VPlayer://assets-library://asset/asset.MOV?id=128AACF1-96E3-4024-85F0-12686DBD54AA&ext=MOV";
+
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:video]];
+
+    NSLog(@"NAL &&& ... %@", video);
 }
 
 
