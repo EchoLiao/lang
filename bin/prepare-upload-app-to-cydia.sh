eb4d827afcc1c06faa0ea3b87200991c2bfcf5e7
nuoerlz <nuoerlz@gmail.com>
2013-01-10 18:49:31 +0800

    add bin/

---
+++ bin/prepare-upload-app-to-cydia.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+wdir=`pwd -P`
+pname=VPlayer
+
+
+rm -rf Payload deb
+
+[[ ! -e "`ls $pname*.ipa`" ]] && {
+    echo "no ipa file found!"
+    exit 1
+}
+[[ ! -e control ]] && {
+    echo "no control file!"
+    exit 1
+}
+
+
+unzip $pname*.ipa
+
+mkdir -p deb
+cd $wdir/deb
+mkdir -p $pname/DEBIAN
+mkdir -p $pname/Applications
+cp -r ../Payload/*.app ./$pname/Applications
+
+cd $wdir
+cp ./control ./deb/$pname/DEBIAN/
+cd $wdir/deb
+dpkg-deb -b $pname
+dpkg-name $pname.deb
+ls -lh $wdir/deb
+
+
+exit 0
