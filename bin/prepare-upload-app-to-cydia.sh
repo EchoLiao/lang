663fd995952e4abe87e915284a668d99650411e8
nuoerlz <nuoerlz@gmail.com>
2013-05-24 16:47:49 +0800

    fix, test file is exist or not

--- bin/prepare-upload-app-to-cydia.sh
+++ bin/prepare-upload-app-to-cydia.sh
@@ -26,24 +26,44 @@ function control_file_example()
     "
 }
 
+function assert_file_exist()
+{
+	[[ ! -e "$1" ]] && {
+		exec >&2
+		echo "file \"$1\" NOT found!"
+		exit 1
+	}
+}
 
 
 
-rm -rf Payload deb
+### main ####
 
 [[ ! -e "`ls $pname*.ipa`" ]] && {
+    exec >&2
     echo "no ipa file found!"
     exit 1
 }
 [[ ! -e control ]] && {
-    echo "no control file!"
+    exec >&2
+    echo "file \"control\" NOT found!"
     control_file_example
     exit 1
 }
+assert_file_exist preinst
+assert_file_exist postinst
+assert_file_exist prerm
+assert_file_exist postrm
+assert_file_exist extrainst_
 
+
+rm -rf Payload deb
+[[ "$1" == "-c" || "$1" == "--clean" ]] && {
+	echo "Clean end."
+	exit 0
+}
 
 . /sw/bin/init.sh
-
 unzip $pname*.ipa
 
 mkdir -p deb
@@ -53,10 +73,18 @@ mkdir -p $pname/Applications
 cp -r ../Payload/*.app ./$pname/Applications
 
 cd $wdir
-cp ./control ./deb/$pname/DEBIAN/
+cp ./control	./deb/$pname/DEBIAN/
+cp preinst		./deb/$pname/DEBIAN/
+cp postinst		./deb/$pname/DEBIAN/
+cp prerm		./deb/$pname/DEBIAN/
+cp postrm		./deb/$pname/DEBIAN/
+cp extrainst_	./deb/$pname/DEBIAN/
+
 cd $wdir/deb
+chmod -R 0755 $pname/DEBIAN
 dpkg-deb -b $pname
 dpkg-name $pname.deb
+
 ls -lh $wdir/deb
 
 
